3e435dfeb418a14a7dac70eb7034456ca3456356
Frank Siyli <frank.siyli@gmail.com>
2023-07-18 23:34:14 +0200

    logo complete

---
+++ src/app/(pages)/profilPage/page.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+//profil
+/* ftp
+HRmax
+weight
+PBs
+old plans*/
+
+// impressum
+//datenschutzerklärung
+
+function Page() {
+  return <div></div>;
+}
+
+export default Page;
--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,24 @@
+"use client";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 import logo from "public/images/logoSmall.png";
+import "./globals.css";
 
 export default function Home() {
+  const router = useRouter();
+  setTimeout(() => {
+    router.push("/homePage");
+  }, 3000);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center ">
       <Image
-        className="scale-150"
+        className="mt-10"
         src={logo}
         alt="logo"
-        width={700}
-        height={700}
+        width={300}
+        height={300}
+        priority
       />
     </main>
   );
