313d9d54165dcd361a7484fd294139622d08332e
Frank Siyli <frank.siyli@gmail.com>
2023-07-19 19:20:29 +0200

    style changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import logo from "../../public/images/logoSmall.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,10 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div className="hero-image"></div>
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
