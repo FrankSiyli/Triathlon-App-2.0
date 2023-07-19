b45172b738f0253aafeb02360ab161b5d5614d6c
Frank Siyli <frank.siyli@gmail.com>
2023-07-19 16:24:55 +0200

    skeleton ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      dark: "#222c37",
+      light: "#f5f4f5",
+      grey: "#a8b0c6",
+      red: "#b4506b",
     },
   },
   plugins: [require("daisyui")],
