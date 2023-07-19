c6f4a17d385c99e26427bed7856f71a9b2137050
Frank Siyli <frank.siyli@gmail.com>
2023-07-19 17:57:57 +0200

    put all to single components

--- src/app/(pages)/homePage/page.js
+++ src/app/(pages)/homePage/page.js
@@ -4,6 +4,58 @@ import "../../globals.css";
 import { v1 as uuidv1 } from "uuid";
 import React from "react";
 
+const Week = ({ week }) => (
+  <div className="collapse collapse-arrow rounded-none mt-2">
+    <input type="checkbox" className="peer" />
+    <div className="collapse-title mb-2 bg-grey text-light text-3xl peer-checked:bg-red peer-checked:text-light ">
+      {week}
+    </div>
+    <div className="collapse-content peer-checked:bg-light peer-checked:text-secondary-content">
+      {days.map((day) => (
+        <Day key={uuidv1()} day={day} />
+      ))}
+    </div>
+  </div>
+);
+
+// Day component
+const Day = ({ day }) => (
+  <div className="collapse collapse-arrow rounded-none mt-4">
+    <input type="checkbox" className="peer" />
+    <div className="collapse-title mb-2 bg-grey text-light text-xl peer-checked:bg-red peer-checked:text-light ">
+      {day}
+    </div>
+    <div className="collapse-content peer-checked:bg-light peer-checked:text-secondary-content">
+      {sessions.map((session) => (
+        <Session key={session.id} session={session} />
+      ))}
+    </div>
+  </div>
+);
+
+// Session component
+const Session = ({ session }) => (
+  <div className="text-center">
+    <div className="collapse collapse-arrow rounded-none mt-4">
+      <input type="checkbox" className="peer" />
+      <div className="collapse-title flex content-around text-center mb-2 bg-grey text-light text-xl peer-checked:bg-red peer-checked:text-light">
+        <div>{session.icon}</div>
+        <div className="ml-2">{session.subTitle}</div>
+      </div>
+      <div className="collapse-content peer-checked:bg-light peer-checked:text-secondary-content">
+        <ul>
+          {session.details.map((detail, index) => (
+            <li key={index} className="text-dark">
+              {detail}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  </div>
+);
+
+// Page component
 function Page() {
   return (
     <>
@@ -11,64 +63,10 @@ function Page() {
         Beispielplan
       </button>
 
-      <div className="flex min-h-screen mb-20 flex-col items-center  p-4">
-        {/*Loop through each week*/}
-
-        {weeks.map((week) => {
-          return (
-            <div
-              key={uuidv1()}
-              className="collapse collapse-arrow rounded-none mt-2"
-            >
-              <input type="checkbox" className="peer" />
-              <div className="collapse-title mb-2 bg-grey text-light text-3xl peer-checked:bg-red peer-checked:text-light ">
-                {week}
-              </div>
-              <div className="collapse-content peer-checked:bg-light peer-checked:text-secondary-content">
-                {/* Loop through each day */}
-                {days.map((day) => {
-                  return (
-                    <div
-                      key={uuidv1()}
-                      className="collapse collapse-arrow rounded-none mt-4"
-                    >
-                      <input type="checkbox" className="peer" />
-                      <div className="collapse-title mb-2 bg-grey text-light text-xl peer-checked:bg-red peer-checked:text-light ">
-                        {day}
-                      </div>
-                      <div className="collapse-content peer-checked:bg-light peer-checked:text-secondary-content">
-                        {/* Loop through sessions and display details for the current day */}
-                        {sessions.map((session) => {
-                          return (
-                            <div key={session.id} className="text-center">
-                              <div className="collapse collapse-arrow rounded-none mt-4">
-                                <input type="checkbox" className="peer" />
-                                <div className="collapse-title flex content-around  text-center mb-2 bg-grey text-light text-xl peer-checked:bg-red peer-checked:text-light">
-                                  <div>{session.icon}</div>
-                                  <div className="ml-2">{session.subTitle}</div>
-                                </div>
-                                <div className="collapse-content peer-checked:bg-light peer-checked:text-secondary-content">
-                                  {/* Display session details */}
-                                  <ul>
-                                    {session.details.map((detail, index) => (
-                                      <li key={index} className="text-dark">
-                                        {detail}
-                                      </li>
-                                    ))}
-                                  </ul>
-                                </div>
-                              </div>
-                            </div>
-                          );
-                        })}
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          );
-        })}
+      <div className="flex min-h-screen mb-20 flex-col items-center p-4">
+        {weeks.map((week) => (
+          <Week key={uuidv1()} week={week} />
+        ))}
       </div>
 
       <Footer />
