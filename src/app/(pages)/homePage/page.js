9e3aa39d6e83bde73b4aaa5343e9bf58114bd834
Frank Siyli <frank.siyli@gmail.com>
2023-07-19 17:18:13 +0200

    add session modal

--- src/app/(pages)/homePage/page.js
+++ src/app/(pages)/homePage/page.js
@@ -2,8 +2,24 @@
 import Footer from "@/app/components/Footer/Footer";
 import "../../globals.css";
 import { v1 as uuidv1 } from "uuid";
+import React from "react";
+import Modal from "react-modal";
 
 function Page() {
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    subtitle.style.color = "black";
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
   return (
     <>
       <button className="btn btn-secondary border-none disabled bg-red m-5 text-2xl">
@@ -42,23 +58,51 @@ function Page() {
                         {sessions.map((session) => {
                           return (
                             <div key={uuidv1()} className="text-center ">
-                              <button className="btn w-full m-2 bg-dark text-light">
-                                {session.subTitle}
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  fill="none"
-                                  viewBox="0 0 24 24"
-                                  strokeWidth={1.5}
-                                  stroke="currentColor"
-                                  className="w-4 h-4 ml-4"
-                                >
-                                  <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                                  />
-                                </svg>
+                              <button
+                                onClick={openModal}
+                                className="btn w-full m-2 bg-dark text-light"
+                              >
+                                <div className="w-full flex justify-around">
+                                  <div>{session.icon}</div>
+                                  <div>{session.subTitle}</div>
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="w-4 h-4 "
+                                  >
+                                    <path
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                                    />
+                                  </svg>
+                                </div>
                               </button>
+                              <Modal
+                                className="flex flex-col justify-center items-center text-dark bg-light w-screen h-screen"
+                                isOpen={modalIsOpen}
+                                onAfterOpen={afterOpenModal}
+                                onRequestClose={closeModal}
+                                ariaHideApp={false}
+                                contentLabel="Example Modal"
+                              >
+                                <h2 ref={(_subtitle) => (subtitle = _subtitle)}>
+                                  Hello
+                                </h2>
+                                <div>I am a modal</div>
+                                <form className="flex flex-col justify-center items-center">
+                                  <button>tab navigation</button>
+                                  <button>stays</button>
+                                  <button>inside</button>
+                                  <button>the modal</button>
+                                  <button className="btn" onClick={closeModal}>
+                                    close
+                                  </button>
+                                </form>
+                              </Modal>
                             </div>
                           );
                         })}
@@ -91,18 +135,23 @@ const sessions = [
   {
     id: uuidv1(),
     title: "run",
+    icon: "🏃‍♀️",
     subTitle: "base run",
     details: ["1k", "5k", "1k"],
   },
   {
     id: uuidv1(),
     title: "swim",
+
+    icon: "🌊",
     subTitle: "base swim",
     details: ["1k", "5k", "1k"],
   },
   {
     id: uuidv1(),
     title: "bike",
+
+    icon: "🚴‍♂️",
     subTitle: "base bike",
     details: ["1k", "5k", "1k"],
   },
