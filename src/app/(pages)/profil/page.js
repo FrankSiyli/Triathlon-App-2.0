50eadc6ccb1a2341a0323eceb95c78eb1b0db54a
Frank Siyli <frank.siyli@gmail.com>
2023-07-21 22:43:32 +0200

    split calculators in single components

--- src/app/(pages)/profil/page.js
+++ src/app/(pages)/profil/page.js
@@ -42,17 +42,29 @@ function Page() {
           </div>
         )}
 
-        <div className="border border-red rounded-md text-center p-3 m-10 bg-dark">
+        <div className="border border-red rounded-md text-center p-3 mt-20  bg-dark">
           <p className="mt-3">Du kennst deinen Maximalpuls nicht?</p>
-          <p>Kein Problem</p>
-          <Link
-            onClick={handleAlertClick}
-            className="btn w-20 m-3 bg-red border border-light text-light"
-            href=""
-          >
-            hier
-          </Link>
-          <p>findest du Informationen.</p>
+          <p>Kein Problem, hier findest du Informationen.</p>
+          <div className="flex flex-col items-center">
+            <Link
+              className="btn w-20 m-3 bg-red border border-light text-light"
+              href="/calculators/heartrate_by_age"
+            >
+              HRmax nach Alter
+            </Link>
+            <Link
+              className="btn w-20 m-3 bg-red border border-light text-light"
+              href="/calculators/heartrate_max"
+            >
+              Puls Zonen
+            </Link>
+            <Link
+              className="btn w-20 m-3 bg-red border border-light text-light"
+              href="/calculators/power_watt"
+            >
+              Watt Zonen
+            </Link>
+          </div>
         </div>
         <div className="mt-20 flex flex-col">
           <Link
