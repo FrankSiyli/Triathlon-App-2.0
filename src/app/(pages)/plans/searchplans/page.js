70e780992a0adedd1c244faeeefefecafe815c7c
Frank Siyli <frank.siyli@gmail.com>
2023-07-28 13:17:02 +0200

    session styling

--- src/app/(pages)/plans/searchplans/page.js
+++ src/app/(pages)/plans/searchplans/page.js
@@ -40,7 +40,7 @@ function Page() {
               />
             </svg>
             {showAlert && (
-              <div className="alert alert-info fixed inset-x-0 inset-y-3 max-w-md h-10 bg-first  flex justify-center ">
+              <div className="alert alert-info fixed inset-x-0 inset-y-3 mx-auto max-w-md h-10 bg-first  flex justify-center ">
                 <span>Coming soon</span>
               </div>
             )}
