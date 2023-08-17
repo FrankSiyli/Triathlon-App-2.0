57018ab8279ad83984cd9fde1599b8a3e2cbf6b4
Frank Siyli <frank.siyli@gmail.com>
2023-08-17 17:54:48 +0200

    add run and bike heartrate zones

--- public/mockDb.js
+++ public/mockDb.js
@@ -1,6 +1,8 @@
 import { formatTime, tresholdsAndPaces } from "@/app/helperFunctions";
 
-const { swim100mPace, runTreshold, bikeTreshold } = tresholdsAndPaces();
+const { swim100mPace, bikeTreshold } = tresholdsAndPaces();
+const userMaxHeartrate = 200;
+const heartrateInput = userMaxHeartrate / 100;
 
 //swim pace zones:
 const swimZ1 = `${formatTime(swim100mPace + 20)} min/100m`; // warm up / cool down
@@ -9,6 +11,43 @@ const swimZ3 = `${formatTime(swim100mPace + 5)} min/100m`; // tempo
 const swimZ4 = `${formatTime(swim100mPace)} min/100m`; // treshold
 const swimZ5 = `${formatTime(swim100mPace - 5)} min/100m`; // anaerobic
 const swimZ6 = `${formatTime(swim100mPace - 10)} min/100m`; // max effort
+
+// run heartrate zones
+const runZ1 = `${Math.round(heartrateInput * 50)} - ${Math.round(
+  heartrateInput * 60
+)} bpm`;
+const runZ2 = `${Math.round(heartrateInput * 60)} - ${Math.round(
+  heartrateInput * 70
+)} bpm`;
+const runZ3 = `${Math.round(heartrateInput * 70)} - ${Math.round(
+  heartrateInput * 80
+)} bpm`;
+const runZ4 = `${Math.round(heartrateInput * 80)} - ${Math.round(
+  heartrateInput * 90
+)} bpm`;
+const runZ5 = `${Math.round(heartrateInput * 90)} - ${Math.round(
+  heartrateInput * 100
+)} bpm`;
+
+// bike heartrate zones
+
+const bikeHeartrateZ1 = `${Math.round(heartrateInput * 45)} - ${Math.round(
+  heartrateInput * 55
+)} bpm`;
+const bikeHeartrateZ2 = `${Math.round(heartrateInput * 55)} - ${Math.round(
+  heartrateInput * 65
+)} bpm`;
+const bikeHeartrateZ3 = `${Math.round(heartrateInput * 65)} - ${Math.round(
+  heartrateInput * 75
+)} bpm`;
+const bikeHeartrateZ4 = `${Math.round(heartrateInput * 75)} - ${Math.round(
+  heartrateInput * 85
+)} bpm`;
+const bikeHeartrateZ5 = `${Math.round(heartrateInput * 85)} - ${Math.round(
+  heartrateInput * 95
+)} bpm`;
+
+// bike watt zones
 
 const examplePlan = {
   name: "Beispielplan",
@@ -33,7 +72,7 @@ const examplePlan = {
                       name: "bla",
                       distance: 20,
                       duration: 0,
-                      zone: swimZ1,
+                      zone: bikeHeartrateZ1,
                     },
                   ],
                 },
