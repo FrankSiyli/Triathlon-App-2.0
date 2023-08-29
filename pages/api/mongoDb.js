7cbfb1cca09a55d48622922ce229bc53ea9394e5
Frank Siyli <frank.siyli@gmail.com>
2023-08-29 16:57:03 +0200

    plans from db are saved to recoil state after fetching from hoepage

--- pages/api/mongoDb.js
+++ pages/api/mongoDb.js
@@ -1,12 +1,18 @@
 import dbConnect from "../../database/dbConnect";
+import Plan from "../../database/models/Plan";
 import Session from "../../database/models/Session";
 
 export default async function handler(request, response) {
   await dbConnect();
 
   if (request.method === "GET") {
-    const sessions = await Session.find();
-    return response.status(200).json(sessions);
+    try {
+      const sessions = await Session.find();
+      const plans = await Plan.find();
+      return response.status(200).json({ sessions, plans });
+    } catch (error) {
+      return response.status(500).json({ message: "Server error" });
+    }
   } else {
     return response.status(405).json({ message: "Method not allowed" });
   }
