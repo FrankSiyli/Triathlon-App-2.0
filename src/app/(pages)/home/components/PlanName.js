68113aabe15fed7f5c6329cd477bfefebc48824a
Frank Siyli <frank.siyli@gmail.com>
2023-08-29 19:14:43 +0200

    plans from db are saved to recoil state after fetching from hoepage

--- src/app/(pages)/home/components/PlanName.js
+++ src/app/(pages)/home/components/PlanName.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const PlanName = ({ boughtUserPlans }) => {
+const PlanName = ({ userPlans }) => {
   return (
     <>
       <button className="btn btn-sm pointer-events-none mx-auto  border-first/50  bg-third m-5  text-first">
-        {boughtUserPlans[0]?.name}
+        {userPlans?.[0]?.name}
       </button>
     </>
   );
