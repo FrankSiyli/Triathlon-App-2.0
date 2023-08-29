2f5e0103523523aaab4d19f042051d14e4a4d8cd
Frank Siyli <frank.siyli@gmail.com>
2023-08-29 17:34:21 +0200

    plans from db are saved to recoil state after fetching from hoepage

--- src/app/(pages)/home/components/PrintSessions.js
+++ src/app/(pages)/home/components/PrintSessions.js
@@ -1,4 +1,4 @@
-import React, { useRef, forwardRef } from "react";
+import React, { forwardRef } from "react";
 import logoBlack from "../../../../../public/images/logoSmallBlack.png";
 import Image from "next/image";
 
@@ -14,8 +14,6 @@ const PrintSessions = forwardRef(
     },
     ref
   ) => {
-    const printComponentRef = useRef();
-
     return (
       <>
         <div className="print-section" ref={ref}>
