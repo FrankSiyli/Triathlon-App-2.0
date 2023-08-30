25202c1c7c9d239b3dfd61acca8b5d38efba47a7
Frank Siyli <frank.siyli@gmail.com>
2023-08-30 20:30:32 +0200

    split mongodb collections for faster fetching

--- pages/api/mongoDb.js
+++ pages/api/mongoDb.js
@@ -1,15 +1,13 @@
 import dbConnect from "../../database/dbConnect";
 import Plan from "../../database/models/Plan";
-import Session from "../../database/models/Session";
 
 export default async function handler(request, response) {
   await dbConnect();
 
   if (request.method === "GET") {
     try {
-      const sessions = await Session.find();
       const plans = await Plan.find();
-      return response.status(200).json({ sessions, plans });
+      return response.status(200).json({ plans });
     } catch (error) {
       return response.status(500).json({ message: "Server error" });
     }
