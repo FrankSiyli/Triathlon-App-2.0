5776dcde81ec40623ae28e24a1ffc32d56694078
Frank Siyli <frank.siyli@gmail.com>
2023-09-05 20:02:54 +0200

    add first yoga instruction image

--- database/models/Plan.js
+++ database/models/Plan.js
@@ -2,6 +2,14 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
+const exerciseSchema = new Schema({
+  name: { type: String },
+  distance: { type: Number },
+  duration: { type: Number },
+  zone: { type: String },
+  imageLink: { type: String },
+});
+
 const planSchema = new Schema({
   name: { type: String },
   price: { type: String },
@@ -19,40 +27,19 @@ const planSchema = new Schema({
               warmUp: [
                 {
                   multiplier: { type: Number },
-                  exercises: [
-                    {
-                      name: { type: String },
-                      distance: { type: Number },
-                      duration: { type: Number },
-                      zone: { type: String },
-                    },
-                  ],
+                  exercises: [exerciseSchema],
                 },
               ],
               main: [
                 {
                   multiplier: { type: Number },
-                  exercises: [
-                    {
-                      name: { type: String },
-                      distance: { type: Number },
-                      duration: { type: Number },
-                      zone: { type: String },
-                    },
-                  ],
+                  exercises: [exerciseSchema],
                 },
               ],
               coolDown: [
                 {
                   multiplier: { type: Number },
-                  exercises: [
-                    {
-                      name: { type: String },
-                      distance: { type: Number },
-                      duration: { type: Number },
-                      zone: { type: String },
-                    },
-                  ],
+                  exercises: [exerciseSchema],
                 },
               ],
             },
