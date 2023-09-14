b26cd2f3fc58fd962da273a104c082390360fc19
Frank Siyli <frank.siyli@gmail.com>
2023-09-14 23:09:33 +0200

    fetch data when needed

--- src/app/page.js
+++ src/app/page.js
@@ -4,15 +4,9 @@ import { useRouter } from "next/navigation";
 import "./globals.css";
 import BackGroundImage from "./components/BackGroundImage/BackGroundImage";
 import useFetchPlans from "./fetchFunctions/useFetchPlans";
-import useFetchSpecialPlans from "./fetchFunctions/useFetchSpecialPlans";
-import useFetchTriathlonPlans from "./fetchFunctions/useFetchTriathlonPlans";
-import useFetchRunPlans from "./fetchFunctions/useFetchRunPlans";
 
 export default function Home() {
   const { error, isLoading } = useFetchPlans();
-  useFetchSpecialPlans();
-  useFetchTriathlonPlans();
-  useFetchRunPlans();
 
   const router = useRouter();
 
