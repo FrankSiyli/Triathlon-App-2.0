718db6ed047b6b2ef472d86fde60e6e4e94f6bd4
Frank Siyli <frank.siyli@gmail.com>
2023-09-14 08:39:16 +0200

    make alert dynamic

--- src/app/components/Alerts/TopAlert.js
+++ src/app/components/Alerts/TopAlert.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Topalert = ({ topAlertText }) => {
+function Topalert({ topAlertText }) {
   return (
     <>
       <div className="fixed mx-auto top-0 inset-x-0 flex flex-row items-center justify-center gap-3 max-w-xl  p-2 rounded-md border border-first/50  bg-alert text-first">
@@ -8,6 +8,6 @@ const Topalert = ({ topAlertText }) => {
       </div>
     </>
   );
-};
+}
 
 export default Topalert;
