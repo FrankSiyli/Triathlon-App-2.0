7f374471b04b2883473c58977885e95ebe46510c
Frank Siyli <frank.siyli@gmail.com>
2023-09-28 20:36:07 +0200

    login and registration done

--- src/app/(pages)/profil/page.js
+++ src/app/(pages)/profil/page.js
@@ -1,3 +1,4 @@
+"use client";
 import Footer from "@/app/components/NavBar/NavBar";
 import React from "react";
 import "../../globals.css";
@@ -5,36 +6,36 @@ import Impressum from "@/app/components/Impressum/Impressum";
 import Image from "next/image";
 import logo from "../../../../public/images/logoSmall.png";
 import LoginAlert from "@/app/components/Alerts/LoginAlert";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import LoginButton from "./components/LoginButton";
 import PersonalZonesButton from "./components/PersonalZonesButton";
 import InformationsButton from "./components/InformationsButton";
 import WelcomeText from "./components/WelcomeText";
 import MyPlansButton from "./components/MyPlansButton";
 import AccountButton from "./components/AccountButton";
+import { useRecoilState } from "recoil";
+import { userNameState } from "@/app/recoil/atoms/user/userNameState";
 
-export default async function Page() {
-  const session = await getServerSession(authOptions);
+export default function Page() {
+  const [userName, setUserName] = useRecoilState(userNameState);
 
   return (
     <>
-      <LoginAlert session={session} />
-      {!session && <div className=" mx-auto w-40 text-center mt-5">Profil</div>}
-      {session && (
+      {/*      <LoginAlert />
+       */}{" "}
+      {userName === "" ? (
+        <div className=" mx-auto w-40 text-center mt-5">Profil</div>
+      ) : (
         <div className=" mx-auto w-40 text-center mt-5">
           <WelcomeText />
         </div>
       )}
-
       <div className=" flex flex-col items-center  mt-10 gap-1  max-w-xl mx-5 ">
-        <LoginButton />
+        {userName === "" ? <LoginButton /> : null}
         <PersonalZonesButton />
         <InformationsButton />
-        <MyPlansButton />
-        <AccountButton />
+        {userName !== "" ? <MyPlansButton /> : null}
+        {userName !== "" ? <AccountButton /> : null}
       </div>
-
       <Image
         priority
         src={logo}
@@ -44,7 +45,6 @@ export default async function Page() {
         height={100}
       />
       <Impressum />
-
       <Footer />
     </>
   );
