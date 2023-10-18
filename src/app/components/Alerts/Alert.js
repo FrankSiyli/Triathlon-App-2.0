813eb695e118f151586065b92f40c297cf0b4f06
Frank Siyli <frank.siyli@gmail.com>
2023-10-18 09:43:56 +0200

    z index bug fix

--- src/app/components/Alerts/Alert.js
+++ src/app/components/Alerts/Alert.js
@@ -3,7 +3,7 @@ import React from "react";
 function Alert({ alertText }) {
   return (
     <>
-      <div className="toast toast-bottom toast-right z-50 ">
+      <div className="toast toast-middle toast-right z-50 ">
         <div className="alert alert-info">
           <span>{alertText}</span>
         </div>
