b0c553d5401314287bbd1061846b431e2559d81e
Frank Siyli <frank.siyli@gmail.com>
2023-10-19 13:42:49 +0200

    overscroll-behavior contain :-)

--- src/app/(pages)/calculators/HeartrateByAge/HeartrateByAge.js
+++ src/app/(pages)/calculators/HeartrateByAge/HeartrateByAge.js
@@ -10,7 +10,7 @@ function HeartrateByAge({ setShowInfos }) {
 
   return (
     <>
-      <div className="w-screen max-w-xl mx-auto">
+      <div className="w-full max-w-xl mx-auto">
         <button
           className="top-5 left-5 btn btn-ghost btn-sm  m-3 border border-transparent text-first "
           onClick={handleBackClick}
