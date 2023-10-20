2116f4b61eba191234f7cdd16d8eae2a1ada763c
Frank Siyli <frank.siyli@gmail.com>
2023-10-20 16:05:42 +0200

    add trainingpeaks logo

--- src/app/(pages)/profil/components/Trainingpeaks/Trainingpeaks.js
+++ src/app/(pages)/profil/components/Trainingpeaks/Trainingpeaks.js
@@ -1,5 +1,7 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import trainingpeaksLogo from "../../../../../../public/images/trainingpeaks_logo_2.png";
 
 const Trainingpeaks = ({ setShowProfil }) => {
   const handleBackClick = () => {
@@ -43,9 +45,15 @@ const Trainingpeaks = ({ setShowProfil }) => {
         <Link
           target="_blank"
           href="https://www.trainingpeaks.com/coach/siyli-endurance-coaching#trainingplans"
-          className="btn btn-sm flex w-40 mx-auto m-5  border border-transparent bg-third  text-first shadow-xl "
+          className="btn btn-sm flex w-40 mx-auto m-5  border border-transparent bg-first  text-first shadow-xl "
         >
-          Trainingpeaks
+          <div className=" -m-2">
+            <Image
+              alt="trainingpeaks"
+              className="w-auto h-10 "
+              src={trainingpeaksLogo}
+            />
+          </div>
         </Link>
       </div>
     </>
