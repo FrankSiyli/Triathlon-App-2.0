cdd9c7fdadf947ad61b447f6937bbde58848562f
Frank Siyli <frank.siyli@gmail.com>
2023-10-22 15:06:11 +0200

    add cookies

--- src/app/(pages)/profil/components/legal/PrivacyPolicy.js
+++ src/app/(pages)/profil/components/legal/PrivacyPolicy.js
@@ -30,62 +30,70 @@ function PrivacyPolicy({ setShowProfil }) {
         </button>
       </div>
 
-      <div className="flex min-h-screen  flex-col  p-4 text-center">
+      <div className="flex mb-20  flex-col  p-4 text-center">
         <p>Datenschutzerklärung</p>
+        <br />
         <p>
           Verantwortlicher im Sinne der Datenschutzgesetze, insbesondere der
           EU-Datenschutzgrundverordnung (DSGVO), ist: Frank Siyli
         </p>
+        <br />
         <p>
           Einzig ausgeführter Google Analytics Parameter ist: Seitenaufrufe.
         </p>
+        <br />
         <p>Ihre Betroffenenrechte</p>
-        Unter den angegebenen Kontaktdaten unseres Datenschutzbeauftragten
-        können Sie jederzeit folgende Rechte ausüben: Auskunft über Ihre bei uns
-        gespeicherten Daten und deren Verarbeitung (Art. 15 DSGVO), Berichtigung
-        unrichtiger personenbezogener Daten (Art. 16 DSGVO), Löschung Ihrer bei
-        uns gespeicherten Daten (Art. 17 DSGVO), Einschränkung der
-        Datenverarbeitung, sofern wir Ihre Daten aufgrund gesetzlicher Pflichten
-        noch nicht löschen dürfen (Art. 18 DSGVO), Widerspruch gegen die
-        Verarbeitung Ihrer Daten bei uns (Art. 21 DSGVO) und
-        Datenübertragbarkeit, sofern Sie in die Datenverarbeitung eingewilligt
-        haben oder einen Vertrag mit uns abgeschlossen haben (Art. 20 DSGVO).
-        Sofern Sie uns eine Einwilligung erteilt haben, können Sie diese
-        jederzeit mit Wirkung für die Zukunft widerrufen. Sie können sich
-        jederzeit mit einer Beschwerde an eine Aufsichtsbehörde wenden, z. B. an
-        die zuständige Aufsichtsbehörde des Bundeslands Ihres Wohnsitzes oder an
-        die für uns als verantwortliche Stelle zuständige Behörde. Eine Liste
-        der Aufsichtsbehörden (für den nichtöffentlichen Bereich) mit Anschrift
-        finden Sie unter:{" "}
+        <br />
+        <p>
+          Unter den angegebenen Kontaktdaten unseres Datenschutzbeauftragten
+          können Sie jederzeit folgende Rechte ausüben: Auskunft über Ihre bei
+          uns gespeicherten Daten und deren Verarbeitung (Art. 15 DSGVO),
+          Berichtigung unrichtiger personenbezogener Daten (Art. 16 DSGVO),
+          Löschung Ihrer bei uns gespeicherten Daten (Art. 17 DSGVO),
+          Einschränkung der Datenverarbeitung, sofern wir Ihre Daten aufgrund
+          gesetzlicher Pflichten noch nicht löschen dürfen (Art. 18 DSGVO),
+          Widerspruch gegen die Verarbeitung Ihrer Daten bei uns (Art. 21 DSGVO)
+          und Datenübertragbarkeit, sofern Sie in die Datenverarbeitung
+          eingewilligt haben oder einen Vertrag mit uns abgeschlossen haben
+          (Art. 20 DSGVO). Sofern Sie uns eine Einwilligung erteilt haben,
+          können Sie diese jederzeit mit Wirkung für die Zukunft widerrufen. Sie
+          können sich jederzeit mit einer Beschwerde an eine Aufsichtsbehörde
+          wenden, z. B. an die zuständige Aufsichtsbehörde des Bundeslands Ihres
+          Wohnsitzes oder an die für uns als verantwortliche Stelle zuständige
+          Behörde. Eine Liste der Aufsichtsbehörden (für den nichtöffentlichen
+          Bereich) mit Anschrift finden Sie unter:
+        </p>
         <Link href="https://www.bfdi.bund.de/DE/Service/Anschriften/Laender/Laender-node.html">
           www.bfdi.bund.de
         </Link>
         <p>Kontaktformular</p>
-        Art und Zweck der Verarbeitung: Die von Ihnen eingegebenen Daten werden
-        zum Zweck der individuellen Kommunikation mit Ihnen gespeichert. Hierfür
-        ist die Angabe einer validen E-Mail-Adresse sowie Ihres Namens
-        erforderlich. Diese dient der Zuordnung der Anfrage und der
-        anschließenden Beantwortung derselben. Die Angabe weiterer Daten ist
-        optional. Rechtsgrundlage: Die Verarbeitung der in das Kontaktformular
-        eingegebenen Daten erfolgt auf der Grundlage eines berechtigten
-        Interesses (Art. 6 Abs. 1 lit. f DSGVO). Durch Bereitstellung des
-        Kontaktformulars möchten wir Ihnen eine unkomplizierte Kontaktaufnahme
-        ermöglichen. Ihre gemachten Angaben werden zum Zwecke der Bearbeitung
-        der Anfrage sowie für mögliche Anschlussfragen gespeichert. Sofern Sie
-        mit uns Kontakt aufnehmen, um ein Angebot zu erfragen, erfolgt die
-        Verarbeitung der in das Kontaktformular eingegebenen Daten zur
-        Durchführung vorvertraglicher Maßnahmen (Art. 6 Abs. 1 lit. b DSGVO).
-        Empfänger: Empfänger der Daten sind ggf. Auftragsverarbeiter.
-        Drittlandtransfer: Die erhobenen Daten werden ggfs. in folgende
-        Drittländer übertragen: nein Folgende Datenschutzgarantien liegen vor:
-        Speicherdauer: Daten werden spätestens 6 Monate nach Bearbeitung der
-        Anfrage gelöscht. Sofern es zu einem Vertragsverhältnis kommt,
-        unterliegen wir den gesetzlichen Aufbewahrungsfristen nach HGB und
-        löschen Ihre Daten nach Ablauf dieser Fristen. Bereitstellung
-        vorgeschrieben oder erforderlich: Die Bereitstellung Ihrer
-        personenbezogenen Daten erfolgt freiwillig. Wir können Ihre Anfrage
-        jedoch nur bearbeiten, sofern Sie uns Ihren Namen, Ihre E-Mail-Adresse
-        und den Grund der Anfrage mitteilen.
+        <p>
+          Art und Zweck der Verarbeitung: Die von Ihnen eingegebenen Daten
+          werden zum Zweck der individuellen Kommunikation mit Ihnen
+          gespeichert. Hierfür ist die Angabe einer validen E-Mail-Adresse sowie
+          Ihres Namens erforderlich. Diese dient der Zuordnung der Anfrage und
+          der anschließenden Beantwortung derselben. Die Angabe weiterer Daten
+          ist optional. Rechtsgrundlage: Die Verarbeitung der in das
+          Kontaktformular eingegebenen Daten erfolgt auf der Grundlage eines
+          berechtigten Interesses (Art. 6 Abs. 1 lit. f DSGVO). Durch
+          Bereitstellung des Kontaktformulars möchten wir Ihnen eine
+          unkomplizierte Kontaktaufnahme ermöglichen. Ihre gemachten Angaben
+          werden zum Zwecke der Bearbeitung der Anfrage sowie für mögliche
+          Anschlussfragen gespeichert. Sofern Sie mit uns Kontakt aufnehmen, um
+          ein Angebot zu erfragen, erfolgt die Verarbeitung der in das
+          Kontaktformular eingegebenen Daten zur Durchführung vorvertraglicher
+          Maßnahmen (Art. 6 Abs. 1 lit. b DSGVO). Empfänger: Empfänger der Daten
+          sind ggf. Auftragsverarbeiter. Drittlandtransfer: Die erhobenen Daten
+          werden ggfs. in folgende Drittländer übertragen: nein Folgende
+          Datenschutzgarantien liegen vor: Speicherdauer: Daten werden
+          spätestens 6 Monate nach Bearbeitung der Anfrage gelöscht. Sofern es
+          zu einem Vertragsverhältnis kommt, unterliegen wir den gesetzlichen
+          Aufbewahrungsfristen nach HGB und löschen Ihre Daten nach Ablauf
+          dieser Fristen. Bereitstellung vorgeschrieben oder erforderlich: Die
+          Bereitstellung Ihrer personenbezogenen Daten erfolgt freiwillig. Wir
+          können Ihre Anfrage jedoch nur bearbeiten, sofern Sie uns Ihren Namen,
+          Ihre E-Mail-Adresse und den Grund der Anfrage mitteilen.
+        </p>
         <p>Information über Ihr Widerspruchsrecht nach Art. 21 DSGVO</p>
         <p>
           Einzelfallbezogenes Widerspruchsrecht Sie haben das Recht, aus
