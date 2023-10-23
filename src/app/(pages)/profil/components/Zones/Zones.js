6e369426017503734978ddc3d889b306afec91b9
Frank Siyli <frank.siyli@gmail.com>
2023-10-23 12:19:39 +0200

    zones buttons design change

--- src/app/(pages)/profil/components/Zones/Zones.js
+++ src/app/(pages)/profil/components/Zones/Zones.js
@@ -57,25 +57,45 @@ function Zones({ setShowProfil }) {
         <div className="relative max-w-xl mt-5">
           <button
             onClick={handleHrClick}
-            className={` btn btn-sm w-28 mx-auto  bg-third text-first shadow-xl  border ${
-              showHrInput
-                ? "border-first border-b-2 border-t-transparent border-r-transparent border-l-transparent"
-                : "border-transparent"
-            }  `}
+            className="flex justify-between w-full max-w-xl shadow-md p-2 rounded-md  my-1 "
           >
-            Maximalpuls
+            <div className="ml-5"> Maximalpuls</div>
+            <svg
+              xmlns="http://w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-5 h-5 mr-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+              />
+            </svg>
           </button>
-          {showHrInput && <HeartrateCalculator />}
           <button
             onClick={handleSwimTimeClick}
-            className={` absolute top-0 right-0 btn btn-sm w-28 mx-auto  bg-third border ${
-              showSwimTimeInput
-                ? "border-first border-b-2 border-t-transparent border-r-transparent border-l-transparent"
-                : "border-transparent"
-            }  text-first shadow-xl `}
+            className="flex justify-between w-full max-w-xl shadow-md p-2 rounded-md  my-1 "
           >
-            Schwimmzeit
+            <div className="ml-5"> Schwimmzeit</div>
+            <svg
+              xmlns="http://w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-5 h-5 mr-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+              />
+            </svg>
           </button>
+          {showHrInput && <HeartrateCalculator />}
           {showSwimTimeInput && <SwimTimeCalculator />}
         </div>
       </div>
