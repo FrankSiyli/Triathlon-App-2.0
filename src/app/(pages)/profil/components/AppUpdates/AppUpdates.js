56385bc0a09da4fbfc2fdd4c20ecc04ca661908d
Frank Siyli <frank.siyli@gmail.com>
2023-10-30 13:03:12 +0100

    fix width bug

--- src/app/(pages)/profil/components/AppUpdates/AppUpdates.js
+++ src/app/(pages)/profil/components/AppUpdates/AppUpdates.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const updates = [
   {
-    name: "einen Triathlonplan hinzugefügt",
+    name: "einen Einsteiger*innen-Triathlonplan hinzugefügt",
     date: "30.10.23",
   },
   {
-    name: "zwei Schwimmpläne hinzugefügt",
+    name: "zwei Aufbau-Schwimmpläne hinzugefügt",
     date: "28.10.23",
   },
   {
