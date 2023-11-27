0872c4249346c56370f0220982f43ce1b933353c
Frank Siyli <frank.siyli@gmail.com>
2023-11-27 16:50:29 +0100

    add header images

--- src/app/(pages)/calendar/Calendar.js
+++ src/app/(pages)/calendar/Calendar.js
@@ -77,10 +77,8 @@ function Calendar() {
                 height={600}
               />
               <div className="h-12 absolute right-0 top-0 w-full  bg-gradient-to-b from-transparent via-transparent via-70% to-background z-10"></div>
-              <div className="h-12 absolute right-0 top-0 w-full  bg-gradient-to-l from-transparent via-transparent via-70% to-background z-10"></div>
-              <div className="h-12 absolute right-0 top-0 w-full  bg-gradient-to-r from-transparent via-transparent via-70% to-background z-10"></div>
 
-              <div className="mx-auto text-center h-10 mt-3 mb-10 rounded-sm px-3 py-1 z-20 bg-gradient-to-b from-transparent  to-background ">
+              <div className="mx-auto text-center h-10 mt-4 mb-10 rounded-sm px-3 py-1 z-20 bg-gradient-to-b from-transparent  to-background ">
                 {homepagePlan?.name}
               </div>
               <WeekScrollButtons
