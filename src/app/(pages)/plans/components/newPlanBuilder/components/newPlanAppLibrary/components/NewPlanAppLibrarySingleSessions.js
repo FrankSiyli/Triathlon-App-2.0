e0cbaa5c4355ec484ddee2d93615e09effc87d66
Frank Siyli <frank.siyli@gmail.com>
2023-12-28 13:25:17 +0100

    add logicfunctions

--- src/app/(pages)/plans/components/newPlanBuilder/components/newPlanAppLibrary/components/NewPlanAppLibrarySingleSessions.js
+++ src/app/(pages)/plans/components/newPlanBuilder/components/newPlanAppLibrary/components/NewPlanAppLibrarySingleSessions.js
@@ -5,6 +5,9 @@ import ArrowDownSvg from "@/app/components/SVGs/arrows/ArrowDownSvg";
 import NewPlanAppLibrarySingleSessionParts from "./NewPlanAppLibrarySingleSessionParts";
 import WatchSvg from "@/app/components/SVGs/WatchSvg";
 import DistanceSvg from "@/app/components/SVGs/DistanceSvg";
+import { formatTime } from "@/app/helperFunctions/formatTime";
+import { calculateTotalDuration } from "@/app/helperFunctions/calculateTotalDuration";
+import { calculateTotalDistance } from "@/app/helperFunctions/calculateTotalDistance";
 
 const NewPlanAppLibrarySingleSessions = ({ singleSessions, sessionType }) => {
   const [showSessionParts, setShowSessionParts] = useState({});
@@ -43,19 +46,30 @@ const NewPlanAppLibrarySingleSessions = ({ singleSessions, sessionType }) => {
                     )}
                   </span>
                 </div>
-                <div className="flex justify-start w-full ml-4 text-xs text-alert gap-3">
-                  <span>
-                    <WatchSvg />
-                  </span>
-                  <span>+</span>
-                  <span>
-                    <DistanceSvg />
-                  </span>
+                <div className="flex justify-start w-full ml-2 mt-1 text-xs text-alert gap-3">
+                  {calculateTotalDuration(singleSession) > 0 ? (
+                    <div className="flex items-center">
+                      <WatchSvg />
+                      {formatTime(calculateTotalDuration(singleSession))}
+                    </div>
+                  ) : null}
+
+                  {calculateTotalDistance(singleSession) > 0 &&
+                  calculateTotalDuration(singleSession) > 0 ? (
+                    <span>+</span>
+                  ) : null}
+
+                  {calculateTotalDistance(singleSession) > 0 ? (
+                    <div className="flex items-center">
+                      <DistanceSvg />
+                      {calculateTotalDistance(singleSession)}m
+                    </div>
+                  ) : null}
                 </div>
               </div>
               {showSessionParts[singleSessionIndex] && (
                 <NewPlanAppLibrarySingleSessionParts
-                  sessionParts={singleSession.sessionParts}
+                  singleSession={singleSession}
                 />
               )}
             </div>
